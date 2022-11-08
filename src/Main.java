1f4bdd049b61ff3203361ba2f57640e8cccd3c48
valeriia-radzivilo <lerazip32@gmail.com>
2022-11-08 17:39:52 +0200

    test updates

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class Main {
        public static void main(String args[]) {
            int n =8;
-            int [] result = make_n_queens.make_n_queens(n,true , new int[]{3, 1, 5, 1, 1, 1, 2, 4});
+            int [] result = make_n_queens.make_n_queens(n,false , new int[]{3,6,2,3,0,5,6,7});
            System.out.println("Result: ");
            ArrWork.create_matr(result,n);
        }
@@ -18,7 +18,7 @@ public class Main {
         void testValueFromMethod()
        {
            int n=8;
-           int []expected = {4, 7, 3, 0, 2, 5, 1, 6};
+           int []expected = {3, 7, 0, 2, 5, 1, 6, 4};
 
            int []result = make_n_queens.make_n_queens(n,false, new int[]{3,6,2,3,0,5,6,7});
            assertArrayEquals(expected,result ,"Test Check");
